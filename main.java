8c134597ece12f622a63f9aa935388a82b063bc7
Mikołaj <32634291+mikpaz@users.noreply.github.com>
2020-10-06 02:05:13 +0200

    main code

---
+++ main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RamkaRysunku ramka=new RamkaRysunku();
+        ramka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ramka.setVisible(true);
+    }
+}
